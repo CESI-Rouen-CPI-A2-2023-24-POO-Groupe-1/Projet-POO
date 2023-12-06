c57683e2a55267b2b2c315f582165e5f6a56678f
Charles P <charles.pingard@viacesi.fr>
2023-12-06 15:34:55 +0100

    :tada: implement splashscreen

--- Splashscreen.h
+++ Splashscreen.h
@@ -15,8 +15,10 @@ namespace ProjetPOO {
 	public ref class Splashscreen : public System::Windows::Forms::Form
 	{
 	public:
-		Splashscreen(void)
+		bool starting;
+		Splashscreen(bool starting)
 		{
+			this->starting = starting;
 			InitializeComponent();
 			//
 			//TODO: ajoutez ici le code du constructeur
@@ -34,6 +36,8 @@ namespace ProjetPOO {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +52,70 @@ namespace ProjetPOO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Splashscreen";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Splashscreen::typeid));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::White;
+			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->ImageLocation = L"";
+			this->pictureBox1->Location = System::Drawing::Point(0, 0);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(600, 400);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// Splashscreen
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(600, 400);
+			this->ControlBox = false;
+			this->Controls->Add(this->pictureBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->MaximizeBox = false;
+			this->MaximumSize = System::Drawing::Size(600, 400);
+			this->MinimizeBox = false;
+			this->MinimumSize = System::Drawing::Size(600, 400);
+			this->Name = L"Splashscreen";
+			this->Opacity = 0;
+			this->ShowIcon = false;
+			this->ShowInTaskbar = false;
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Splashscreen";
+			this->TransparencyKey = System::Drawing::Color::White;
+			this->Shown += gcnew System::EventHandler(this, &Splashscreen::splash_shown);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void splash_shown(System::Object^ sender, System::EventArgs^ e) {
+		while (this->Opacity < 1){
+			this->Opacity += 0.01;
+			this->Refresh();
+			System::Threading::Thread::Sleep(1);
+		}
+		if (this->starting){
+			// App is starting
+			System::Threading::Thread::Sleep(1000);
+		}
+		else {
+			// App is closing
+			System::Threading::Thread::Sleep(500);
+		}
+		while (this->Opacity > 0){
+			this->Opacity -= 0.05;
+			this->Refresh();
+			System::Threading::Thread::Sleep(1);
+		}
+		this->Close();
+	}
 	};
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 // Projet POO CESI 2023
 
-int main() {
-	return 0;
+#include "Splashscreen.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int __clrcall WinMain(array<String^>^ args )
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ProjetPOO::Splashscreen splashscreen;
+	Application::Run(%splashscreen);
 }
