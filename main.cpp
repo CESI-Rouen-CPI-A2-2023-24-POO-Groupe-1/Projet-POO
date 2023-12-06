17742b249a96d8c68236d8884be1859db8e4a38d
LeSeptembre <115709711+LeSeptembre@users.noreply.github.com>
2023-12-06 15:53:32 +0100

    :sparkles: Classe de connexion bdd opérationnelle 🗿🗿🗿🗿🗿🗿

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 // Projet POO CESI 2023
-
+#include "DataBase.h"
+using namespace std;
+using namespace NS_Composants;
 int main() {
+	DataBase Feur;
+	Feur.actionRows("use NORTICBDD; INSERT INTO Pays (PAYS_NOM) VALUES ('France');");
+	cout << "fin de requete" << endl;
 	return 0;
 }
