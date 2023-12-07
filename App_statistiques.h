411e8c54510d813a6a9c45309acc90cdcf60c999
MaxenceDCesi <maxence.david@viacesi.fr>
2023-12-07 16:00:17 +0100

    Ajouter des images (certaines vont changer)

--- App_statistiques.h
+++ App_statistiques.h
@@ -18,9 +18,12 @@ namespace ProjetPOO {
 		App_statistiques(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: ajoutez ici le code du constructeur
-			//
+			this->textBox1->Enter += gcnew System::EventHandler(this, &App_statistiques::textBox1_Enter);
+			this->textBox1->Leave += gcnew System::EventHandler(this, &App_statistiques::textBox1_Leave);
+			this->textBox2->Enter += gcnew System::EventHandler(this, &App_statistiques::textBox2_Enter);
+			this->textBox2->Leave += gcnew System::EventHandler(this, &App_statistiques::textBox2_Leave);
+			this->textBox3->Enter += gcnew System::EventHandler(this, &App_statistiques::textBox3_Enter);
+			this->textBox3->Leave += gcnew System::EventHandler(this, &App_statistiques::textBox3_Leave);
 		}
 
 	protected:
@@ -35,11 +38,38 @@ namespace ProjetPOO {
 			}
 		}
 
+	protected:
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ Main_Titre_2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+
+	private: System::Windows::Forms::DataGridView^ dataGridView2;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+	private: System::Windows::Forms::TextBox^ textBox3;
+
+	private:
+		void SetInitialText() {
+			textBox1->Text = "Identificateur Client";
+			textBox2->Text = "Prénom";
+			textBox3->Text = "Nom";
+		}
+
+	protected:
+
+	protected:
+
+	protected:
+
 	private:
 		/// <summary>
 		/// Variable nécessaire au concepteur.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +78,250 @@ namespace ProjetPOO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"App_statistiques";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(App_statistiques::typeid));
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->Main_Titre_2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::ButtonFace;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(1092, 322);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(720, 452);
+			this->dataGridView1->TabIndex = 2;
+			// 
+			// label1
+			// 
+			this->label1->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Calibri Light", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->label1->Location = System::Drawing::Point(1314, 143);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(399, 37);
+			this->label1->TabIndex = 10;
+			this->label1->Text = L"Pôle de gestion des statistiques";
+			// 
+			// Main_Titre_2
+			// 
+			this->Main_Titre_2->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->Main_Titre_2->AutoSize = true;
+			this->Main_Titre_2->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->Main_Titre_2->Font = (gcnew System::Drawing::Font(L"Calibri", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Main_Titre_2->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->Main_Titre_2->Location = System::Drawing::Point(1193, 143);
+			this->Main_Titre_2->Name = L"Main_Titre_2";
+			this->Main_Titre_2->Size = System::Drawing::Size(126, 37);
+			this->Main_Titre_2->TabIndex = 9;
+			this->Main_Titre_2->Text = L"NORTIC :";
+			// 
+			// button1
+			// 
+			this->button1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.BackgroundImage")));
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->button1->Location = System::Drawing::Point(1785, 64);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(114, 61);
+			this->button1->TabIndex = 11;
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &App_statistiques::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->textBox1->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox1->ForeColor = System::Drawing::SystemColors::ButtonShadow;
+			this->textBox1->Location = System::Drawing::Point(1092, 283);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(220, 22);
+			this->textBox1->TabIndex = 12;
+			this->textBox1->Text = L"Identificateur client";
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &App_statistiques::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->textBox2->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox2->ForeColor = System::Drawing::SystemColors::ControlDark;
+			this->textBox2->Location = System::Drawing::Point(1342, 283);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(220, 22);
+			this->textBox2->TabIndex = 13;
+			this->textBox2->Text = L"Prénom";
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &App_statistiques::textBox2_TextChanged);
+			// 
+			// textBox3
+			// 
+			this->textBox3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->textBox3->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox3->ForeColor = System::Drawing::SystemColors::ControlDark;
+			this->textBox3->Location = System::Drawing::Point(1592, 283);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(220, 22);
+			this->textBox3->TabIndex = 14;
+			this->textBox3->Text = L"Nom";
+			this->textBox3->TextChanged += gcnew System::EventHandler(this, &App_statistiques::textBox3_TextChanged);
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->BackgroundColor = System::Drawing::SystemColors::ButtonFace;
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->Location = System::Drawing::Point(176, 181);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->RowHeadersWidth = 51;
+			this->dataGridView2->RowTemplate->Height = 24;
+			this->dataGridView2->Size = System::Drawing::Size(780, 700);
+			this->dataGridView2->TabIndex = 16;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(1079, 268);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(744, 517);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 17;
+			this->pictureBox2->TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->pictureBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(161, 165);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(811, 732);
+			this->pictureBox1->TabIndex = 15;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &App_statistiques::pictureBox1_Click);
+			// 
+			// App_statistiques
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::Window;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(1924, 923);
+			this->Controls->Add(this->dataGridView2);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->Main_Titre_2);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->pictureBox2);
+			this->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::SizableToolWindow;
+			this->Name = L"App_statistiques";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
+			this->Text = L"App_statistiques";
+			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
+			this->Load += gcnew System::EventHandler(this, &App_statistiques::App_statistiques_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void App_statistiques_Load(System::Object^ sender, System::EventArgs^ e) {
+		SetInitialText();
+	}
+	private: System::Void listBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+
+	private: System::Void textBox1_Enter(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox1->Text == "Identificateur Client") {
+			textBox1->Text = "";
+			textBox1->ForeColor = System::Drawing::SystemColors::ControlText;
+		}
+	}
+
+	private: System::Void textBox1_Leave(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox1->Text == "") {
+			textBox1->Text = "Identificateur Client";
+			textBox1->ForeColor = System::Drawing::SystemColors::ButtonShadow;
+		}
+	}
+
+	private: System::Void textBox2_Enter(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox2->Text == "Prénom") {
+			textBox2->Text = "";
+			textBox2->ForeColor = System::Drawing::SystemColors::ControlText;
+		}
+	}
+
+	private: System::Void textBox2_Leave(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox2->Text == "") {
+			textBox2->Text = "Prénom";
+			textBox2->ForeColor = System::Drawing::SystemColors::ControlDark;
+		}
+	}
+
+	private: System::Void textBox3_Enter(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox3->Text == "Nom") {
+			textBox3->Text = "";
+			textBox3->ForeColor = System::Drawing::SystemColors::ControlText;
+		}
+	}
+
+	private: System::Void textBox3_Leave(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox3->Text == "") {
+			textBox3->Text = "Nom";
+			textBox3->ForeColor = System::Drawing::SystemColors::ControlDark;
+		}
+	}
+	private: System::Void dataGridView2_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	}
+	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
