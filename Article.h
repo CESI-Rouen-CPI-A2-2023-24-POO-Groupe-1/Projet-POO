77b5ccae3020180ee9521f5be31cd48eef293578
LeSeptembre <115709711+LeSeptembre@users.noreply.github.com>
2023-12-07 14:26:06 +0100

    :sparkles: Ajout Article 🗿🗿🗿

---
+++ Article.h
@@ -0,0 +1,24 @@
+#include "Tax.h"
+#include "DataBase.h"
+
+using namespace System;
+
+ref class Article {
+private:
+	int id;
+	String^ name;
+	float price;
+	Tax^ tax;
+public:
+	Article::Article(String^ name, float price, Tax^ tax);
+	int Article::getId();
+	String^ Article::getName();
+	void Article::setName(String^ name);
+	float Article::getPrice();
+	void Article::setPrice(float price);
+	Tax^ Article::getTax();
+	void Article::setTax(Tax tax);
+
+
+
+};
